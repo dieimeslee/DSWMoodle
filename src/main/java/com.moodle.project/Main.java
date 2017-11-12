25bf263c4cccf22c73d9669bfa21e22c44b88dfa
Arthur Geron <arthurgeron@hotmail.com>
2017-11-11 23:33:49 -0200

    Fixed Main.java

--- src/main/java/com.moodle.project/Main.java
+++ src/main/java/com.moodle.project/Main.java
@@ -1,19 +1,17 @@
 package com.moodle.project;
 
-import java.util.Optional;
 import org.apache.catalina.startup.Tomcat;
+import java.util.*;
 
 public class Main {
     public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
+    public static final Optional<String> env = Optional.ofNullable(System.getenv("ENV"));
     public static void main(String[] args) throws Exception {
-        String port = "8080", contextPath = "/", appBase = ".";
-        Map<String, String> env = System.getenv();
-        for (String envName : env.keySet())
-            if (envName.equals("ENV"))
-                if (env.get(envName).equals("production"))
-                    port = "80";
+        String _port = port.orElse("8080"), contextPath = "/", appBase = ".";
+        if ("production".equals(env.orElse("development")))
+            _port = "80";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.valueOf(port.orElse("8080") ));
+        tomcat.setPort(Integer.valueOf(_port));
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
